d9868055be23cbc2c7c7683b57964269c9b2eb36
Monica02007 <192524315.simats@saveetha.com>
2026-09-03 11:59:06 +0530

    Set base path for ticket booking system

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ticket-booking-system-/',
   plugins: [react()],
   server: {
     host: '0.0.0.0',
     port: 3000,
     allowedHosts: true,
   },
-});
+})
